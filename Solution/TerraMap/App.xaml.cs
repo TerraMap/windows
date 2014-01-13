eadf168b8f89cb48cbf66ac0142c42d1be4c2f10
jcoon_cp <SND\jcoon_cp>
2014-01-13 05:49:00 -0800

    Adding command-line rendering, not yet complete.

--- Solution/TerraMap/App.xaml.cs
+++ Solution/TerraMap/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
@@ -11,6 +12,55 @@ namespace TerraMap
 {
 	public partial class App : Application
 	{
+		[DllImport("Kernel32.dll")]
+		public static extern bool AttachConsole(int processId);
+
+		[DllImport("kernel32.dll")]
+		public static extern Boolean FreeConsole();
+
+		public Options Options { get; private set; }
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			this.Options = new Options();
+
+			var commandLineParsed = CommandLine.Parser.Default.ParseArguments(e.Args, this.Options);
+
+			if (AttachConsole(-1))
+			{
+				Console.WriteLine("Attach debugger now and press any key...");
+				Console.ReadKey();
+
+				Console.WriteLine("");
+
+				try
+				{
+					if (this.Options.NoGui)
+					{
+						this.RenderInConsole();
+					}
+				}
+				finally
+				{
+					Console.WriteLine("Press a key to quit");
+
+					FreeConsole();
+
+					this.Shutdown();
+				}
+			}
+			else
+			{
+				new MainWindow().Show();
+			}
+		}
+
+		private void RenderInConsole()
+		{
+		}
+
 		private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
 		{
 			MessageBox.Show(e.Exception.ToString());
---
+++ Solution/TerraMap/Options.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraMap
+{
+	public class Options
+	{
+		[Option('i', "input", Required = true, HelpText = "Input wld file to be processed.")]
+		public string InputFile { get; set; }
+
+		[Option('o', "output", Required = true, HelpText = "Output png file to be created/overwritten.")]
+		public string OutputFile { get; set; }
+
+		[Option('n', "nogui", DefaultValue = false, HelpText = "Do not display any user interface.")]
+		public bool NoGui { get; set; }
+
+		[ParserState]
+		public IParserState LastParserState { get; set; }
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			return HelpText.AutoBuild(this, (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+		}
+	}
+}
