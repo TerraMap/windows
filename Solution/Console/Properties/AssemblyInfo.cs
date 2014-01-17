20fb554f0ee2fb7018b523d4df4afb1e8a9409f1
jcoon_cp <SND\jcoon_cp>
2014-01-17 06:22:09 -0800

    Added TerraMapCmd.exe to the TerraMap project, for headless, unattended world map image generation, complete with highlight capability. Incremented version to 1.0.1.

--- Solution/Console/Properties/AssemblyInfo.cs
+++ Solution/Console/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TerraMap.Console")]
+[assembly: AssemblyTitle("TerraMapCmd")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jason Coon")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
