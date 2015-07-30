368240c526b31c7f1d58e1f48d2a19de5b131484
pup05 <jasoncoon@hotmail.com>
2015-07-29 19:24:10 -0500

    Added search box to world properties window.

--- TerraMap/WorldPropertiesWindow.xaml.cs
+++ TerraMap/WorldPropertiesWindow.xaml.cs
@@ -11,14 +11,56 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TerraMap.Data;
 
 namespace TerraMap
 {
 	public partial class WorldPropertiesWindow : OwnedWindow
 	{
+		CollectionViewSource collectionViewSource;
+
 		public WorldPropertiesWindow()
 		{
 			InitializeComponent();
+
+			collectionViewSource = (CollectionViewSource)this.FindResource("collectionViewSource");
+		}
+
+		private void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			this.searchBox.Focus();
+		}
+
+		private void OnFilter(object sender, FilterEventArgs e)
+		{
+			e.Accepted = false;
+
+			var item = e.Item as WorldProperty;
+			if (item == null)
+				return;
+
+			e.Accepted = item.Name.ToLower().Contains(this.searchBox.Text.ToLower());
+		}
+
+		private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+		{
+			this.collectionViewSource.View.Refresh();
+			this.collectionViewSource.View.MoveCurrentToFirst();
+		}
+
+		private void OnSearchBoxClear(object sender, RoutedEventArgs e)
+		{
+			this.searchBox.Text = "";
+		}
+
+		private void OnCanExecuteClose(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void OnExecutedClose(object sender, ExecutedRoutedEventArgs e)
+		{
+			this.Close();
 		}
 	}
 }
